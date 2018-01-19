a465805a37e80a55541202daa14668aeedadc836
Anbarasi U <anbarasiu@gmail.com>
2018-01-19 22:24:00 +0530

    Endpoint + CORS

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 import xmltodict
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 def convert(xml_file, xml_attribs=True):
     with open(xml_file, "rb") as f:
         d = xmltodict.parse(f, xml_attribs=xml_attribs)
-        return json.dumps(d, indent=4)
+        return json.dumps(d)
 
-@app.route('/')
+@app.route('/v1/books/')
 def hello():
     return convert("mock.xml")
 
