14ed851dcf3f9f75164df4b0a7a8c392d43447a8
Anbarasi U <anbarasiu@gmail.com>
2018-01-23 22:15:23 +0530

    Connect to API

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 import xmltodict
 import json
+import requests
 
 app = Flask(__name__)
 CORS(app)
-
+# undefined entity - from some chars in goodreads response
 def convert(xml_file, xml_attribs=True):
-    with open(xml_file, "rb") as f:
-        d = xmltodict.parse(f, xml_attribs=xml_attribs)
-        return json.dumps(d)
+    # with open(xml_file, "rb") as f:
+    f = requests.get('https://www.goodreads.com/review/list/11879872.xml?key=key&v=2&id=anbarasiu&shelf=to-read&per_page=200')
+    d = xmltodict.parse(f.text, xml_attribs=xml_attribs)
+    return json.dumps(d['GoodreadsResponse']['reviews'])
 
 @app.route('/v1/books/')
 def hello():
